c9b22015ca687bb39876736c32fbb2cae62dc551
Gourabdasg <dasg69171@gmail.com>
2026-09-25 13:08:30 +0530

    Configure official Firebase Web Options in firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,18 +1,30 @@
 // File generated for Firebase project: disha-saathi (Disha saathi)
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for project disha-saathi.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
     if (defaultTargetPlatform == TargetPlatform.android) {
       return android;
     }
     throw UnsupportedError(
-      'DefaultFirebaseOptions are only configured for Android currently. '
-      'Run `flutterfire configure` to add Web/iOS support.',
+      'DefaultFirebaseOptions are configured for Android and Web currently.',
     );
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyC54N8OIgiiqJSmX2GBATYXeMTqkHEOHo',
+    appId: '1:610010711984:web:260b8c9b65805ec2e43875',
+    messagingSenderId: '610010711984',
+    projectId: 'disha-saathi',
+    authDomain: 'disha-saathi.firebaseapp.com',
+    storageBucket: 'disha-saathi.firebasestorage.app',
+    measurementId: 'G-57PLBXT6P0',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAytmixk6ZZ0dQgpn6-xw_C2K0gcyYQ3nI',
